5653604f1d38695ce3ca60d60aa3b15f3cfa0cab
Matheus <matheusbj95@gmail.com>
2024-11-13 00:04:26 -0300

    feat: função de editar tarefa funcionando

--- atividade5/src/js/script.js
+++ atividade5/src/js/script.js
@@ -1,10 +1,31 @@
+let tarefas = []
+
 function adicionarTarefa() {
     const inputTarefa = document.getElementById('input-tarefa');
     const tarefa = inputTarefa.value.trim();
     if (tarefa === '') return;
-    const listaTarefas = document.getElementById('lista-tarefa');
-    const novaTarefa = document.createElement('li');
-    novaTarefa.innerHTML = `<span contenteditable="false">${tarefa}</span>`;
-    listaTarefas.appendChild(novaTarefa);
+    tarefas.push(tarefa)
+    atualizarListaTarefas();
     inputTarefa.value = '';
+}
+
+function atualizarListaTarefas() {
+    const listaTarefas = document.getElementById('lista-tarefa');
+    listaTarefas.innerHTML = '';
+    tarefas.forEach((tarefa, index) => {
+        const novaTarefa = document.createElement('li');
+        novaTarefa.innerHTML = `
+            <span contenteditable="false">${tarefa}</span>
+            <button onclick="editarTarefa(${index})">Editar</button>
+        `;
+        listaTarefas.appendChild(novaTarefa);
+    });
+}
+
+function editarTarefa(index) {
+    const novoTexto = prompt("Edite a tarefa:", tarefas[index]);
+    if (novoTexto !== null && novoTexto.trim() !== '') {
+        tarefas[index] = novoTexto.trim(); 
+        atualizarListaTarefas();
+    }
 }
